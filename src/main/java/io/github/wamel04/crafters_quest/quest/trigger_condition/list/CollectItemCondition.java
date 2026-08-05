5971803c9b73b78d4036927f296906c082b49649
BuildTools <unconfigured@null.spigotmc.org>
2026-08-05 19:46:24 +0900

    CraftersQuest 1.1
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/io/github/wamel04/crafters_quest/quest/trigger_condition/list/CollectItemCondition.java
+++ src/main/java/io/github/wamel04/crafters_quest/quest/trigger_condition/list/CollectItemCondition.java
@@ -18,9 +18,19 @@ import io.github.wamel04.crafters_quest.quest.trigger_condition.TriggerCondition
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CollectItemCondition extends ProgressTriggerCondition {
+
+    private static final Map<UUID, Long> lastCheckTimeMap = new ConcurrentHashMap<>();
+    private static final long CHECK_COOLDOWN = 100L;
 
     // COLLECT_ITEM 'SLIME_BALL' '30'
     public CollectItemCondition(String symbol, TriggerConditionType type) {
@@ -31,94 +41,106 @@ public class CollectItemCondition extends ProgressTriggerCondition {
                         PacketType.Play.Server.SET_SLOT, PacketType.Play.Server.WINDOW_ITEMS) {
                     @Override
                     public void onPacketSending(PacketEvent event) {
-                        Player player = event.getPlayer();
+                        updateItemProgress(event.getPlayer());
+                    }
+                }
+        );
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+                new PacketAdapter(CraftersQuestPlugin.getInstance(), ListenerPriority.LOWEST,
+                        PacketType.Play.Client.SET_CREATIVE_SLOT) {
+                    @Override
+                    public void onPacketReceiving(PacketEvent event) {
+                        UUID uuid = event.getPlayer().getUniqueId();
 
-                        Bukkit.getScheduler().runTask(CraftersQuestPlugin.getInstance(), () -> {
-                            updateItemProgress(player);
-                        });
+                        long currentTime = System.currentTimeMillis();
+
+                        lastCheckTimeMap.put(uuid, currentTime);
                     }
                 }
         );
     }
 
-//    @EventHandler
-//    public void onPickup(EntityPickupItemEvent event) {
-//        if (event.isCancelled())
-//            return;
-//        if (!(event.getEntity() instanceof Player))
-//            return;
-//
-//        updateItemProgress((Player) event.getEntity());
-//    }
-//
-//    @EventHandler
-//    public void onDrop(PlayerDropItemEvent event) {
-//        if (event.isCancelled())
-//            return;
-//
-//        updateItemProgress(event.getPlayer());
-//    }
-//
-//    @EventHandler
-//    public void onInventoryClick(InventoryClickEvent event) {
-//        if (event.isCancelled())
-//            return;
-//        if (!(event.getWhoClicked() instanceof Player))
-//            return;
-//
-//        updateItemProgress((Player) event.getWhoClicked());
-//    }
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent event) {
+        if (event.isCancelled())
+            return;
+        if (!(event.getEntity() instanceof Player))
+            return;
+
+        updateItemProgress((Player) event.getEntity());
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        if (event.isCancelled())
+            return;
+
+        updateItemProgress(event.getPlayer());
+    }
+
 
     private void updateItemProgress(Player player) {
-        Bukkit.getScheduler().runTaskLater(CraftersQuestPlugin.getInstance(), () -> {
-            if (player.isOnline()) {
-                QuestDataContainer.get(player.getUniqueId().toString())
-                        .thenAcceptAsync(qdc -> {
-                            for (Quest quest : qdc.getProceedingQuests()) {
-                                for (QuestCondition questCondition : quest.getQuestConditionMap().values()) {
-                                    if (!questCondition.getTriggerCondition().getSymbol().equalsIgnoreCase(getSymbol()))
-                                        continue;
+        UUID uuid = player.getUniqueId();
 
-                                    String condStr = questCondition.getTriggerConditionString();
-                                    String cItemName = getFactorMap(condStr, "itemName", "amount").get("itemName").toLowerCase();
+        long currentTime = System.currentTimeMillis();
+        long lastTime = lastCheckTimeMap.getOrDefault(uuid, 0L);
 
-                                    int currentAmount = 0;
-                                    for (ItemStack item : player.getInventory().getContents()) {
-                                        if (item == null || item.getType().isAir())
-                                            continue;
+        if (currentTime - lastTime < CHECK_COOLDOWN)
+            return;
 
-                                        if (isItemMatched(item, cItemName))
-                                            currentAmount += item.getAmount();
-                                    }
+        lastCheckTimeMap.put(uuid, currentTime);
 
-                                    QuestData questData = qdc.getQuestDataMap().get(quest.getId());
-                                    QuestConditionData condData = questData.getQuestConditionDataMap().get(questCondition.getId());
-                                    int currentProgress = condData.getCurrentProgress();
+        QuestDataContainer qdc = QuestDataContainer.questDataContainerMap.get(player.getUniqueId().toString());
+        if (qdc == null)
+            return;
 
-                                    if (currentAmount > currentProgress) {
-                                        qdc.progressQuestCondition(player, questCondition, currentAmount - currentProgress);
-                                    }
-                                    if (currentAmount < currentProgress) {
-                                        if (condData.isCompleted()) {
-                                            condData.reset();
-                                        }
+        for (Quest quest : qdc.getProceedingQuests()) {
+            for (QuestCondition questCondition : quest.getQuestConditionMap().values()) {
+                if (!questCondition.getTriggerCondition().getSymbol().equalsIgnoreCase(getSymbol()))
+                    continue;
 
-                                        condData.setCurrentProgress(currentAmount);
+                String condStr = questCondition.getTriggerConditionString();
+                String cItemName = getFactorMap(condStr, "itemName", "amount").get("itemName").toLowerCase();
 
-                                        if (!questCondition.getProgressOperations().isEmpty()) {
-                                            questCondition.getProgressOperationStrings().stream().forEach(ops -> {
-                                                Operation operation = Operation.parseOperation(ops);
+                Bukkit.getScheduler().runTask(CraftersQuestPlugin.getInstance(), () -> {
+                    int currentAmount = 0;
+                    for (ItemStack item : player.getInventory().getContents()) {
+                        if (item != null && isItemMatched(item, cItemName)) {
+                            currentAmount += item.getAmount();
+                        }
+                    }
+                    ItemStack cursorItem = player.getItemOnCursor();
 
-                                                if (operation != null)
-                                                    operation.execute(player, ops, questCondition);
-                                            });
-                                        }
-                                    }
-                                }
-                            }
-                        });
+                    if (!cursorItem.getType().isAir()) {
+                        if (isItemMatched(cursorItem, cItemName)) {
+                            currentAmount += cursorItem.getAmount();
+                        }
+                    }
+
+                    QuestData questData = qdc.getQuestDataMap().get(quest.getId());
+                    QuestConditionData condData = questData.getQuestConditionDataMap().get(questCondition.getId());
+                    int currentProgress = condData.getCurrentProgress();
+
+                    if (currentAmount > currentProgress) {
+                        qdc.progressQuestCondition(player, questCondition, currentAmount - currentProgress);
+                    } else if (currentAmount < currentProgress) {
+                        if (condData.isCompleted()) {
+                            condData.reset();
+                        }
+
+                        condData.setCurrentProgress(currentAmount);
+
+                        if (!questCondition.getProgressOperations().isEmpty()) {
+                            questCondition.getProgressOperationStrings().forEach(ops -> {
+                                Operation operation = Operation.parseOperation(ops);
+                                if (operation != null)
+                                    operation.execute(player, ops, questCondition);
+                            });
+                        }
+                    }
+                });
             }
-        }, 1);
+        }
     }
 
     private boolean isItemMatched(ItemStack currentItem, String cItemName) {
